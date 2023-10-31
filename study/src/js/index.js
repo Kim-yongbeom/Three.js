2f480773dd031b8573220907b409bd6de5c41132
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2023-10-31 17:48:55 +0900

    Update index.js

--- study/src/js/index.js
+++ study/src/js/index.js
@@ -1,34 +1,48 @@
 import * as THREE from "three";
 
-const $result = document.getElementById('result');
+const $result = document.getElementById("result");
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffe287)
+scene.background = new THREE.Color(0xffe287);
 
 const camera = new THREE.PerspectiveCamera(
-    50,
-    $result.clientWidth/$result.clientHeight,
-    0.1,
-    1000
-)
-camera.position.set(2,2,2);
-camera.lookAt(0,0,0)
+  50,
+  $result.clientWidth / $result.clientHeight,
+  0.1,
+  1000
+);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: $result,
-    antialias: true
+  canvas: $result,
+  antialias: true,
 });
 renderer.setSize($result.clientWidth, $result.clientHeight);
 document.body.appendChild(renderer.domElement);
 
 const light = new THREE.DirectionalLight(0xffffff);
-light.position.set(2,4,3)
+light.position.set(2, 4, 3);
 scene.add(light);
 
-const geomety = new THREE.BoxGeometry(1,1,1);
+const geomety = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-    color: 0x2E6FF2
-})
+  color: 0x2e6ff2,
+});
 const box = new THREE.Mesh(geomety, material);
 scene.add(box);
-renderer.render(scene, camera)
+renderer.render(scene, camera);
+
+function animate() {
+  box.rotation.y += 0.01;
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
